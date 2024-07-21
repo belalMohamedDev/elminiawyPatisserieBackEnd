cc76b81040a52e7a4fc4a1794d247b8a4c090c10
belal mohamed <belalagwa0@gmail.com>
2024-07-21 22:07:48 +0200

    done testing to reduis

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -1,30 +1,4 @@
-// const { createClient } = require('redis');
-
-// const redisClient = createClient({
-//   password: process.env.REDIS_PASSWORD || "",
-//   socket: {
-//     host: process.env.REDIS_HOST || "127.0.0.1",
-//     port: process.env.REDIS_PORT || 6379,
-//   },
-// });
-
-// redisClient.on('connect', () => {
-//   console.log('Connected to Redis');
-// });
-
-// redisClient.on('error', (err) => {
-//   console.error('Redis connection error:', err);
-// });
-
-// // Connect to Redis server
-// redisClient.connect();
-
-
-
-// module.exports = redisClient;
-
-
-const { createClient } = require("redis");
+const { createClient } = require('redis');
 
 const redisClient = createClient({
   password: process.env.REDIS_PASSWORD || "",
@@ -34,25 +8,19 @@ const redisClient = createClient({
   },
 });
 
-redisClient.on("connect", () => {
-  console.log("Connected to Redis");
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
 });
 
-redisClient.on("error", (err) => {
-  console.error("Redis connection error:", err);
+redisClient.on('error', (err) => {
+  console.error('Redis connection error:', err);
 });
 
 // Connect to Redis server
 redisClient.connect();
 
-(async () => {
-  try {
-    // Use the async method for flushing the database
-    const success = await redisClient.flushAll();
-    console.log("Flush all successful:", success);
-  } catch (err) {
-    console.error("Error flushing Redis database:", err);
-  }
-})();
+
 
 module.exports = redisClient;
+
+
