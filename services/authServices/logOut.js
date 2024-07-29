0f8ef401ab95d8e33e370230711253bb6490c146
belal mohamed <belalagwa0@gmail.com>
2024-07-30 00:20:28 +0200

    update log out service

--- services/authServices/logOut.js
+++ services/authServices/logOut.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
 const i18n = require("i18n");
 
 const ApiError = require("../../utils/apiError/apiError");
@@ -7,23 +9,33 @@ const userModel = require("../../modules/userModel");
 // @ dec log out
 // @ route Post  /api/vi/auth/logout
 // @ access Public
+
 exports.logOut = asyncHandler(async (req, res, next) => {
   const { refreshToken } = req.body;
   if (!refreshToken) {
-    return next(new ApiError(i18n.__("refreshTokeRequired"), 400));
+    return next(new ApiError(i18n.__("refreshTokenRequired"), 400));
   }
 
-  const user = await userModel.findOneAndUpdate(
-    // { refreshToken },
-    { _id: req.userModel._id },
-    { refreshToken: null },
-    { new: true }
-  );
-  if (!user) {
+  let decoded;
+  try {
+    decoded = jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_TOKEN_SECRET_KEY
+    );
+  } catch (err) {
     return next(new ApiError(i18n.__("invalidRefreshToken"), 400));
   }
 
-  res.status(201).json({
+  const user = await userModel.findOne({ _id: decoded.userId });
+
+  if (!user || user.refreshToken !== refreshToken) {
+    return next(new ApiError(i18n.__("invalidRefreshToken"), 400));
+  }
+
+  user.refreshToken = null;
+  await user.save();
+
+  res.status(200).json({
     status: true,
     message: i18n.__("loggedOutSuccessfully"),
   });
