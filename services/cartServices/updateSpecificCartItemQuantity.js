8e7e9555c6b22fbd0fda9f8a5be816c1c889456a
belal mohamed <belalagwa0@gmail.com>
2024-08-12 21:24:45 +0200

    localization updateSpecificCartItemQuantity

--- services/cartServices/updateSpecificCartItemQuantity.js
+++ services/cartServices/updateSpecificCartItemQuantity.js
@@ -1,46 +1,54 @@
-const asyncHandler = require('express-async-handler')
-const CartModel = require('../../modules/cartModel')
-const ApiError = require('../../utils/apiError/apiError')
-
+const asyncHandler = require("express-async-handler");
+const CartModel = require("../../modules/cartModel");
+const ApiError = require("../../utils/apiError/apiError");
+const i18n = require("i18n");
+const ProductModel = require("../../modules/productModel");
 
 //  @dec    update specific Cart item quantity
 //  @route  Put  /api/v1/cart/:itemId
 //  @access Private/user
 exports.updateSpecificCartItemQuantity = asyncHandler(
-    async (req, res, next) => {
-      const { quantity } = req.body;
-  
-      const cart = await CartModel.findOne({ user: req.userModel._id });
-  
-      if (!cart) {
-        return next(
-          new ApiError(`there is no cart for this user id : ${req.userModel.id}`, 404)
-        );
-      }
-  
-      const itemIndex = cart.cartItems.findIndex(
-        (item) => item._id.toString() === req.params.itemId
-      );
-      if (itemIndex > -1) {
-      
-        cart.cartItems[itemIndex].quantity += quantity;
+  async (req, res, next) => {
+    const { quantity } = req.body;
 
-      } else {
-        return next(
-          new ApiError(
-            `there is no item for this  id : ${req.params.itemId}`,
-            404
-          )
-        );
-      }
-  
-      await cart.save();
-  
-      res.status(200).json({
-        status: true,
-        message: "Successfully updated quantity of item in cart",
-        numOfCartItems: cart.cartItems.length,
-        data: cart,
-      });
+    if (quantity === 0) {
+      return next(new ApiError(i18n.__("quantityShouldBeAtLeast"), 400));
     }
-  );
+
+    const cart = await CartModel.findOne({ user: req.userModel._id });
+
+    if (!cart) {
+      return next(new ApiError(i18n.__("cartNotFound"), 404));
+    }
+
+    const itemIndex = cart.cartItems.findIndex(
+      (item) => item._id.toString() === req.params.itemId
+    );
+
+    if (itemIndex > -1) {
+      if (cart.cartItems[itemIndex].quantity > 0) {
+        cart.cartItems[itemIndex].quantity += quantity;
+      } else {
+        // Remove the item from the cart
+        cart.cartItems[itemIndex].quantity = 1;
+      }
+    } else {
+      return next(new ApiError(i18n.__("itemNotFoundInTheCart"), 404));
+    }
+
+    await cart.save();
+
+    // Helper function to localize product data
+    var localizedDocument = ProductModel.schema.methods.toJSONLocalizedOnly(
+      cart,
+      req.headers["lang"] || "en"
+    );
+
+    res.status(200).json({
+      status: true,
+      message: i18n.__("successfullyUpdatedQuantityOfItemInCart"),
+      numOfCartItems: cart.cartItems.length,
+      data: localizedDocument,
+    });
+  }
+);
