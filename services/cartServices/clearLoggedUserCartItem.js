85484629a8fa38e5b9f1c72e919fa3fa4aec27ca
belal mohamed <belalagwa0@gmail.com>
2024-08-31 20:28:09 +0300

    update clearLoggedUserCartItem

--- services/cartServices/clearLoggedUserCartItem.js
+++ services/cartServices/clearLoggedUserCartItem.js
@@ -1,26 +1,29 @@
-const asyncHandler = require('express-async-handler')
-const CartModel = require('../../modules/cartModel')
+const asyncHandler = require("express-async-handler");
+const CartModel = require("../../modules/cartModel");
 const i18n = require("i18n");
-
 
 //  @dec    clear logged Cart item
 //  @route  Delete  /api/v1/cart/clearItem/:itemId
 //  @access Private/user
 exports.clearLoggedUserCartItem = asyncHandler(async (req, res, next) => {
   const cart = await CartModel.findOneAndDelete(
-      { user: req.userModel._id },
-      {
-        $pull: { cartItems: { _id: req.params.itemId } },
-      },
-      {
-        new: true,
-      }
-    );
-  
-    res.status(200).json({
-    
-      status: true,
-      message: i18n.__("theCartIsNowEmpty"),
-      data: cart,
-    });
+    { user: req.userModel._id },
+    {
+      $pull: { cartItems: { _id: req.params.itemId } },
+    },
+    {
+      new: true,
+    }
+  );
+
+  // If the cart doesn't exist, return an error
+  if (!cart) {
+    return next(new ApiError(i18n.__("cartNotFound"), 404));
+  }
+
+  res.status(200).json({
+    status: true,
+    message: i18n.__("theCartIsNowEmpty"),
+    data: cart,
   });
+});
