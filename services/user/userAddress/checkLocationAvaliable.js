42ba698b8a74d4f6f05d5f64bf67e0fdafff86be
belal mohamed <belalagwa0@gmail.com>
2024-10-14 20:41:44 +0300

    add checkLocationAvailable function localization arabic and english

--- services/user/userAddress/checkLocationAvaliable.js
+++ services/user/userAddress/checkLocationAvaliable.js
@@ -5,48 +5,54 @@ const i18n = require("i18n");
 
 // @dec Check if location is available or not based on store's delivery range
 // @route POST /api/v1/address/isAvailable
-// @access Protected
+// @access public
 
 exports.checkLocationAvailable = asyncHandler(async (req, res, next) => {
   const { latitude, longitude } = req.body;
 
-  const locale = req.headers["lang"] || "en";
-
   const apiKey = process.env.GOOGLE_API_KEY;
 
-  // Prepare the URL for Google Maps API request
-
-  const googleApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}&language=${locale}`;
+  // Prepare the URL for Google Maps API request for both languages
+  const googleApiUrlEn = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}&language=en`;
+  const googleApiUrlAr = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}&language=ar`;
 
   const userLocation = {
     type: "Point",
     coordinates: [parseFloat(longitude), parseFloat(latitude)],
   };
 
-  // Run both the store search and Google Maps API request in parallel
-  const [stores, googleApiResponse] = await Promise.all([
-    await storeAddressModel.findOne({
+  // Run both the store search and Google Maps API requests in parallel
+  const [stores, googleApiResponseEn, googleApiResponseAr] = await Promise.all([
+    storeAddressModel.findOne({
       deliveryZone: {
         $geoIntersects: {
           $geometry: userLocation,
         },
       },
     }),
-    axios.get(googleApiUrl), // Fetch the address from Google API
+    axios.get(googleApiUrlEn), // Fetch the address in English from Google API
+    axios.get(googleApiUrlAr), // Fetch the address in Arabic from Google API
   ]);
 
-  // Extract the formatted address from Google API response
-  const address =
-    googleApiResponse.data.results[0]?.formatted_address ||
+  // Extract the formatted addresses from Google API responses
+  const addressEn =
+    googleApiResponseEn.data.results[0]?.formatted_address ||
     i18n.__("addressNotFound");
-  const isAddressAvailable = address !== i18n.__("addressNotFound"); // true if address is found, false if not
+  const addressAr =
+    googleApiResponseAr.data.results[0]?.formatted_address ||
+    i18n.__("addressNotFound");
+
+  const isAddressAvailable =
+    addressEn !== i18n.__("addressNotFound") &&
+    addressAr !== i18n.__("addressNotFound");
 
   // Check if any stores are available within the delivery range
   if (!stores) {
     return res.status(200).json({
       status: true,
       message: i18n.__("noStoresAvailableWithinTheSpecifiedDistance"),
-      address: address,
+      englishAddress: addressEn,
+      arabicAddress: addressAr,  
       StoreAddressAvailable: false,
       isAddressAvailable: isAddressAvailable,
     });
@@ -55,9 +61,9 @@ exports.checkLocationAvailable = asyncHandler(async (req, res, next) => {
   return res.status(200).json({
     status: true,
     message: i18n.__("storeFoundWithinTheSpecifiedDistance"),
-    address: address,
+    englishAddress: addressEn,
+    arabicAddress: addressAr,  
     StoreAddressAvailable: true,
     isAddressAvailable: isAddressAvailable,
   });
 });
-
