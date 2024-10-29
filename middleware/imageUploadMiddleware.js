e1363ea94d2f849b7f8ee80b113f85876da5f93f
belal mohamed <belalagwa0@gmail.com>
2024-10-29 21:07:53 +0300

    change multerOption

--- middleware/imageUploadMiddleware.js
+++ middleware/imageUploadMiddleware.js
@@ -1,35 +1,26 @@
 const multer = require("multer");
 const ApiError = require("../utils/apiError/apiError");
 
-
-  
-const multerOption=()=>{
-    const multerStorage = multer.memoryStorage();
-    //memory storage
+const multerOption = () => {
+  const multerStorage = multer.memoryStorage();
+  //memory storage
   const multerFilter = function (req, file, cb) {
-    
-    if (file.mimetype.startsWith("image")) {
-      cb(null, true);
-    } else {
+    const ext = path.extname(file.originalname);
+
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
       cb(new ApiError("only images are allowed", 400), false);
+    } else {
+      cb(null, true);
     }
   };
-  
+
   const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
-  
-  return upload
-  }
-  
-  
-const uploadSingleImage=(fieldName)=> multerOption().single(fieldName);
-  
-  
-  
-  
-const uploadListOfImage=(arryOfFields)=> multerOption().fields(arryOfFields);
 
+  return upload;
+};
 
-  module.exports={uploadSingleImage,uploadListOfImage}
+const uploadSingleImage = (fieldName) => multerOption().single(fieldName);
 
+const uploadListOfImage = (arryOfFields) => multerOption().fields(arryOfFields);
 
-
+module.exports = { uploadSingleImage, uploadListOfImage };
